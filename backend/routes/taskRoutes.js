83c06e34b0ef73279eca8608880ccb4df3a2df0c
shaahamad-syed <shaahamadsyed@gmail.com>
2026-01-10 20:37:38 +0530

    checking

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const taskController = require("../controllers/taskController");
-
-// router.post("/", taskController.createTask);
-// router.get("/", taskController.getTasks);
-// router.put("/:id", taskController.updateTask);
-// router.delete("/:id", taskController.deleteTask);
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();
